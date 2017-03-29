e1b363fef303091fdd44dae92272c75174f55d3b
Jason Rodriguez <jason.eli.rodriguez@gmail.com>
2017-03-29 11:49:43 -0600

    added database tools

---
+++ keystroke.js
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////////
+//  TODO:                                                   //
+// 1. Develop Test Cases                                    //
+// 2. Format Code                                           //
+// 3. Create a real-time authenticator                      //
+// 4. Create documentation of the tool                      //
+//////////////////////////////////////////////////////////////
+module.exports = function() {
+const path = require('path');
+var getDatabaseResults = true; //set this to true to pull all the data from the database. WARNING!!! It will take a long time so plan on leaving it running for 30 minutes or so.
+var databaseAccess = require(path.join(__dirname, "lib", "database-access", "db_access.js"));
+var arrayOfDataObjects = databaseAccess(getDatabaseResults);
+}
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,47 @@
-var express = require('express');
+const express = require('express');
+const path = require('path');
+var fs = require('fs');
+var archiver = require('archiver');
+
+const keystroke_interpreter = require(path.join(__dirname, "..", 'keystroke.js'));
+const keystroke_database = require(path.join(__dirname, "..", "lib", "database-access", "justGetData.js"));
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
-router.get('/download-database', function(req, res, next) {
-  console.log('./');
+router.post('/api/v1/download-database', function (req, res, next) {
+  keystroke_database();
+});
+
+router.get('/api/v1/get-raw-data', function (req, res, next) {
+
+  var output = fs.createWriteStream('raw.zip');
+  var archive = archiver('zip');
+
+  output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+    res.sendFile('raw.zip');
+  });
+
+  archive.on('error', function (err) {
+    throw err;
+  });
+
+  archive.pipe(output);
+  archive.bulk([{
+    expand: true,
+    cwd: 'consent_forms',
+    src: ['**', '../users/**', '../raw/**'],
+    dest: 'consent_forms'
+  }]);
+  archive.finalize();
 });
 
 
